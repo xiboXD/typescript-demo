9fa5222003dc15130ac6c707e8b12b3023cee873
aelf-lu <yimeng.lu@aelf.io>
2024-01-04 19:16:34 +0800

    fix

--- contract/AElf.Contracts.HelloWorldContract/src/HelloWorldState.cs
+++ contract/AElf.Contracts.HelloWorldContract/src/HelloWorldState.cs
@@ -6,7 +6,6 @@ using AElf.Types;
 namespace AElf.Contracts.HelloWorld
 {
     // The state class is used to communicate with the blockchain.
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
     public class HelloWorldState : ContractState
     {
         // StringState is used to put the single data.
